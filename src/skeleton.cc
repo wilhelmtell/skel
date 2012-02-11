4d8f654263be3cb4e99be5224c3f1ecca67690f3
Matan Nassau <matan.nassau@gmail.com>
2012-02-11 16:20:12 -0500

    copy_r()

--- src/skeleton.cc
+++ src/skeleton.cc
@@ -11,40 +11,26 @@
 
 namespace fs = boost::filesystem3;
 
-struct raii_chdir {
-    raii_chdir(fs::path const& p);
-    ~raii_chdir();
-private:
-    fs::path orig;
-};
-
-raii_chdir::raii_chdir(fs::path const& p)
-: orig(fs::current_path())
+namespace {
+void copy_r(fs::path const& from, fs::path const& to)
 {
-    TEMPLOG_LOG(skel::log_developer,templog::sev_debug,templog::aud_developer)
-        << "chdir " << p.string();
-    fs::current_path(p);
-}
-
-raii_chdir::~raii_chdir()
-{
-    TEMPLOG_LOG(skel::log_developer,templog::sev_debug,templog::aud_developer)
-        << "chdir " << orig.string();
-    fs::current_path(orig);
-}
-
-void copy_r(fs::path const& from)
-{
-    auto original_cwd(fs::current_path());
-    raii_chdir temporarily_chdir(fs::path(getenv("HOME")) / skel::RC_DIR);
+    auto const from_parent = from.parent_path();
+    auto const parent_path_length = std::distance(from_parent.begin(),
+                                                  from_parent.end());
     std::for_each(fs::recursive_directory_iterator(from),
                   fs::recursive_directory_iterator(),
                   [&](fs::directory_entry const& ent) {
         TEMPLOG_LOG(skel::log_developer,templog::sev_debug,templog::aud_developer)
             << "copying " << ent.path().string();
-        fs::copy(ent.path(), original_cwd / ent.path());
+        auto const current = ent.path();
+        auto const relative_pos = std::next(current.begin(),
+                                            parent_path_length + 1);
+        auto const current_to = std::accumulate(relative_pos, current.end(),
+                                                to, std::divides<fs::path>());
+        fs::copy(current, current_to);
     });
 }
+}  // namespace
 
 namespace skel {
 skeleton::skeleton(std::string const& name)
@@ -56,23 +42,7 @@ void skeleton::instantiate() const
 {
     TEMPLOG_LOG(log_developer,templog::sev_info,templog::aud_developer)
         << "instantiating skeleton " << name << " ...";
-    // fs::copy(fs::path(getenv("HOME")) / RC_DIR / name, name);
-    // copy_r(name);
-    auto const ABS_RC_DIR = fs::path(getenv("HOME")) / RC_DIR;
-    fs::path const SKELETON_DIR(ABS_RC_DIR / name);
-    std::for_each(fs::recursive_directory_iterator(SKELETON_DIR),
-                  fs::recursive_directory_iterator(),
-                  [&](fs::directory_entry const& ent) {
-        TEMPLOG_LOG(skel::log_developer,templog::sev_debug,templog::aud_developer)
-            << "copying " << ent.path().string();
-        auto const current = ent.path();
-        auto const relative_pos = std::next(current.begin(),
-                                            std::distance(ABS_RC_DIR.begin(),
-                                                          ABS_RC_DIR.end()) + 1);
-        auto const p = std::accumulate(relative_pos, current.end(),
-                                       fs::path(), std::divides<fs::path>());
-        fs::copy(current, p);
-    });
+    copy_r(fs::path(getenv("HOME")) / RC_DIR / name, fs::current_path());
 }
 
 bool skeleton_installed(std::string const& name)
