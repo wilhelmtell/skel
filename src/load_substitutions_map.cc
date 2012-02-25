fb720cc79ddf6202f82cb0dde7bba5aef35eb435
Matan Nassau <matan.nassau@gmail.com>
2012-02-25 17:10:44 -0500

    load_substitutions_map(): s/^\s*//

--- src/load_substitutions_map.cc
+++ src/load_substitutions_map.cc
@@ -2,16 +2,19 @@
 #include <map>
 #include <string>
 #include <iostream>
-
-struct bad_substitutions_map { };
+#include <algorithm>
+#include <cctype>
 
 namespace skel {
 void load_substitutions_map(std::istream& in,
                             std::map<std::string,std::string>& subs)
 {
-    for( std::string name, value; in; ) {
-        if( !(in >> name) ) throw bad_substitutions_map();
-        std::getline(in, value);
+    for( std::string name, value; in >> name && std::getline(in, value); ) {
+        auto const non_space_pos = std::find_if(value.begin(), value.end(), 
+                                                [](char c) {
+                                                    return ! std::isspace(c);
+                                                });
+        value.erase(value.begin(), non_space_pos);
         subs[name] = value;
     }
 }
