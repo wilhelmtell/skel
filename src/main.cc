7366dd8b6f45162645d81ec8951e7ee92190e351
Matan Nassau <matan.nassau@gmail.com>
2012-03-31 14:15:34 -0400

    accept substitution mappings from commandline
    ./skel APPNAME:skel VERSION:0.1 <file.txt

--- src/main.cc
+++ src/main.cc
@@ -2,14 +2,30 @@
 #include "parser.hh"
 #include <string>
 #include <map>
+#include <algorithm>
+#include <iterator>
 
-int main(int /*argc*/, char* /*argv*/[])
+std::map<std::string,std::string> parse_substitutions(int count, char * pairs[])
 {
-    std::map<std::string,std::string> substitutions{
-        {"APPNAME", "skel"},
-        {"VERSION", "0.1"}
-    };
+    std::map<std::string,std::string> substitutions;
+    for( int i = 0; i != count; ++i ) {
+        std::string pair = pairs[i];
+        auto const delim_pos = std::find(pair.begin(), pair.end(), ':');
+        if( delim_pos == pair.end() )
+            return substitutions;
+        std::string from(pair.begin(), delim_pos);
+        std::string to(std::next(delim_pos), pair.end());
+        substitutions.insert(std::make_pair(from, to));
+    }
+    return substitutions;
+}
+
+int main(int argc, char* argv[])
+{
     try {
+        std::map<std::string,std::string> substitutions{
+            parse_substitutions(argc - 1, &argv[1])
+        };
         skel::parse(std::cin, std::cout, substitutions);
     } catch( skel::syntax_error const& e) {
         std::cerr << "syntax error: " << e.what() << '\n';
