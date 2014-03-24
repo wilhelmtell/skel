027c459c9dedd66c08d65a43ff63d07ba92df159
Matan Nassau <matan.nassau@gmail.com>
2014-03-23 20:31:27 -0400

    c++ac: add test/dummy.cc

---
+++ dot_skel/c++ac/test/dummy.cc
@@ -0,0 +1,6 @@
+#include <catch/catch.hpp>
+#include <{APPNAME}/dummy.hh>
+
+TEST_CASE( "fundamental number theory" ) \{
+    REQUIRE( 0 != 1 );
+\}
--- dot_skel/c++ac/test/main.cc
+++ dot_skel/c++ac/test/main.cc
@@ -1,7 +1,2 @@
 #define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
-#include <{APPNAME}/dummy.hh>
-
-TEST_CASE( "fauborg transposes splines" ) \{
-    REQUIRE( 0 != 1 );
-\}
